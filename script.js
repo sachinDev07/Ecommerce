e69f883219ef122a7a52419f7b945f4d837051a2
Sachin <sachinkrj077@gmail.com>
2022-09-16 13:52:33 +0500

    || Added Javascript ||

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+//   ----------- Navbar -----------
+
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
@@ -12,4 +15,23 @@ if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active');
     })
+}
+
+
+//   ----------- Single Product -----------
+
+var mainImg = document.getElementById('mainImg');
+var smallImg = document.getElementsByClassName('small-img');
+
+smallImg[0].onclick = function() {
+    mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function() {
+    mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function() {
+    mainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function() {
+    mainImg.src = smallImg[3].src;
 }
