6a5ed1ad83a79aa1694ca60f3a9d0de21b09f5a3
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-09 23:34:47 +0500

    done Navbar

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,12 +1,41 @@
 const { Router } = require('express');
 const express = require('express');
 
+const NavDetail = require('../models/NavDetail')
+
 const routes = express.Router();
 
-routes.get('/', (req, resp) => {
-    resp.render('index')
+routes.get('/', async (req, resp) => {
+    const navDetail = await NavDetail.findOne({'_id':'63937e8661d2e752ba892e2f'});
+
+    resp.render('index',{
+        navDetail:navDetail
+    })
 })
 
+// routes.get('/shop', (req, resp) => {
+//     resp.render('shop')
+// })
+
+// routes.get('/about', (req, resp) => {
+//     resp.render('about')
+// })
+
+// routes.get('/blog', (req, resp) => {
+//     resp.render('blog')
+// })
+
+// routes.get('/contact', (req, resp) => {
+//     resp.render('contact')
+// })
+
+// routes.get('/cart', (req, resp) => {
+//     resp.render('cart')
+// })
+
+// routes.get('/sproduct', (req, resp) => {
+//     resp.render('sproduct')
+// })
 
 
 
