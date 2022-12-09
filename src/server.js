ead0be08d1e1d4026016ffd3b183702ecc999c75
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-09 23:36:18 +0500

    done Navbar

--- src/server.js
+++ src/server.js
@@ -1,20 +1,66 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const hbs = require("hbs");
 
-const routes = require('./routes/main')
+const NavDetail = require("./models/NavDetail");
+
+const routes = require("./routes/main");
 
 const app = express();
-const PORT = process.env.PORT || 5300
+const PORT = process.env.PORT || 5300;
 
-app.use('',routes);
+// for accessing static files
+app.use("/static", express.static("public"));
+app.use("", routes);
 
+mongoose.set("strictQuery", true);
 
 // (template engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+app.set("view engine", "hbs");
+app.set("views", "views");
+hbs.registerPartials("views/partials"); // setting partials
 
+// database connection
+mongoose.connect(
+  "mongodb://0.0.0.0:27017/shopping-ecom-website",
+  async (err) => {
+    if (err) {
+      console.log("Unable to connect with the database");
+    } else {
+    //   NavDetail.create({
+    //     brandIcon: "/static/images/logo.png",
+    //     cartIcon: "fa-solid fa-bag-shopping",
+    //     cartUrl: "/cart",
+    //     links: [
+    //       {
+    //         label: "Home",
+    //         url: "/",
+    //       },
+    //       {
+    //         label: "Shop",
+    //         url: "/shop",
+    //       },
+    //       {
+    //         label: "About",
+    //         url: "/about",
+    //       },
+    //       {
+    //         label: "Blog",
+    //         url: "/blog",
+    //       },
+    //       {
+    //         label: "Contact",
+    //         url: "/contact",
+    //       },
+    //     ],
+        
+    //   });
 
+      console.log("Database connected");
+    }
+  }
+);
 
 app.listen(PORT, () => {
-    console.log(`Server connected on port ${PORT}`);
+  console.log(`Server connected on port ${PORT}`);
 });
-
