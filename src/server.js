8d096caadf3d80917aeeb3e707fcba4862ff4def
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-10 01:36:26 +0500

    done hero section schema details

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const hbs = require("hbs");
 
-const NavDetail = require("./models/NavDetail");
+const NavDetail = require("./models/home/NavDetail");
+const Hero = require('./models/home/Hero');
 
 const routes = require("./routes/main");
+const hero = require("./models/home/Hero");
 
 const app = express();
 const PORT = process.env.PORT || 5300;
@@ -27,34 +29,47 @@ mongoose.connect(
     if (err) {
       console.log("Unable to connect with the database");
     } else {
-    //   NavDetail.create({
-    //     brandIcon: "/static/images/logo.png",
-    //     cartIcon: "fa-solid fa-bag-shopping",
-    //     cartUrl: "/cart",
-    //     links: [
-    //       {
-    //         label: "Home",
-    //         url: "/",
-    //       },
-    //       {
-    //         label: "Shop",
-    //         url: "/shop",
-    //       },
-    //       {
-    //         label: "About",
-    //         url: "/about",
-    //       },
-    //       {
-    //         label: "Blog",
-    //         url: "/blog",
-    //       },
-    //       {
-    //         label: "Contact",
-    //         url: "/contact",
-    //       },
-    //     ],
-        
-    //   });
+        // NavDetail.create({
+        //   brandIcon: "/static/images/logo.png",
+        //   cartIcon: "fa-solid fa-bag-shopping",
+        //   cartUrl: "/cart",
+        //   links: [
+        //     {
+        //       label: "Home",
+        //       url: "/",
+        //     },
+        //     {
+        //       label: "Shop",
+        //       url: "/shop",
+        //     },
+        //     {
+        //       label: "About",
+        //       url: "/about",
+        //     },
+        //     {
+        //       label: "Blog",
+        //       url: "/blog",
+        //     },
+        //     {
+        //       label: "Contact",
+        //       url: "/contact",
+        //     },
+        //   ],
+
+        // });
+
+
+
+      // hero section
+      
+      // hero.create({
+      //   bgImage:"/static/images/hero4.png",
+      //   link:"/cart",
+      //   titleOne:"Super value deals",
+      //   titleTwo:"On all products",
+      //   subTitle:"Trade-in-offer",
+      //   paragraph:"Save more with coupons & up to 70% off!",
+      // })
 
       console.log("Database connected");
     }
