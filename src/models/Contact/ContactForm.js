0f6e64355fbc7bca193eef5da3bc7d3b04c71032
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2022-12-14 11:28:00 +0530

    Packages shortlisted and fixed Schema Format.

--- src/models/Contact/ContactForm.js
+++ src/models/Contact/ContactForm.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 
 const ContactSchema = new mongoose.Schema({
-    name:String,
-    email:String,
-    subject:String,
-    message:String,
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    subject: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+    },
 });
 
-module.exports = mongoose.model('contact-form',ContactSchema);
+module.exports = mongoose.model('contact-form', ContactSchema);
